d9ed55ba8e256bb77b9230c7ccf6f8848ffed159
BHaskarMishra05 <bhaskarmishra1590@gmail.com>
2025-12-22 10:28:56 +0530

    project structure

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+from typing import List 
+import os 
+import sys
+
+setup(
+    name="DiBetPRed101",
+    version="0.0.0",
+    author="BHASKARMISHRA",
+    author_email="bhaskarmishra1590@gmail.com",
+    packages=find_packages(),
+)
